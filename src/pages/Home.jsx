c820f702c0a633e06085ae9a0211b4d747696fbb
allaiar <radzhapov.allayar@gmail.com>
2023-05-10 14:54:08 +0600

    mini react project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
-import { Route, Routes, Link } from "react-router-dom";
+import {Route, Link } from "react-router-dom";
 
 const Home = ({products}) => {
   return (
